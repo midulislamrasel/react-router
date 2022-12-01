d6a9f68fc77f7d7b7c5c6dc14cb84666425da870
Midul-islam-Russell <midulraddy121@gmail.com>
2022-12-01 22:39:19 +0600

    footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './component/Header/Header';
+import Footer from './component/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header></Header>
+        <Footer></Footer>
       </header>
     </div>
   );
