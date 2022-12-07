ae3c30dd3b97849bceac743c319cb523bb18c991
Midul-islam-Russell <midulraddy121@gmail.com>
2022-12-07 11:35:00 +0600

    add

--- src/component/Card/Card.js
+++ src/component/Card/Card.js
@@ -4,6 +4,7 @@ const Card = () => {
     return (
         <div>
             <h4>Hello Card</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, delectus?</p>
         </div>
     );
 };
