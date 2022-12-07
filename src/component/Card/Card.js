04e9f57f01b4073c002c7e73a1db151def358143
Midul-islam-Russell <midulraddy121@gmail.com>
2022-12-07 11:34:00 +0600

    add

---
+++ src/component/Card/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <h4>Hello Card</h4>
+        </div>
+    );
+};
+
+export default Card;
--- src/component/Main/Main.js
+++ src/component/Main/Main.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Shop from '../Shop/Shop';
+// import { Link } from 'react-router-dom';
 
 const Main = () => {
     return (
         <div>
+            <div>
                 <a href="About">About</a>
                 <a href="Footer">Footer</a>
                 <a href="Header">Header</a>
                 <a href="Order">Order</a>
                 <a href="Shop">Shop</a>
+            </div>
+            {/* <div>
+                <Link to="About">About</Link>
+                <Link to="Footer">Footer</Link>
+                <Link to="Header">Header</Link>
+                <Link to="Order">Order</Link>
+                <Link to={Shop}>Shop</Link>
+            </div> */}
         </div>
     );
 };
