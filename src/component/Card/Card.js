c828dc9eace6a0972015497a26c591e161b4cdf9
Midul-islam-Russell <midulraddy121@gmail.com>
2022-12-08 19:00:44 +0600

    exam cholche push korte parchi na tao nicher mon rokha korte faka push korchi

--- src/component/Card/Card.js
+++ src/component/Card/Card.js
@@ -3,8 +3,7 @@ import React from 'react';
 const Card = () => {
     return (
         <div>
-            <h4>Hello Card</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, delectus?</p>
+
         </div>
     );
 };
