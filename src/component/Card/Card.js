b9623ddb6193e9278770dfa9cdd7ef1dd39d64aa
Midul-islam-Russell <midulraddy121@gmail.com>
2022-12-09 11:25:55 +0600

    পরীক্ষার জন্য সময় দিতে পারছি না তার পরেও মন কে বুজে দেওয়ার জন্য ফাঁকা পুশ করছি

--- src/component/Card/Card.js
+++ src/component/Card/Card.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Card = () => {
     return (
         <div>
-
+            <h4>Hello Card</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, delectus?</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, quaerat?</p>
         </div>
     );
 };
