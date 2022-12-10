db220418997cc27e2542aa2ddc8a93c66ac23d32
Midul-islam-Russell <midulraddy121@gmail.com>
2022-12-10 20:28:31 +0600

    পরীক্ষার জন্য সময় দিতে পারছি না তার পরেও মন কে বুজে দেওয়ার জন্য ফাঁকা… … পুশ করছি

--- src/component/Card/Card.js
+++ src/component/Card/Card.js
@@ -7,6 +7,8 @@ const Card = () => {
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, delectus?</p>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, quaerat?</p>\
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem totam praesentium, distinctio quibusdam expedita laudantium accusamus tempore laborum quaerat, non temporibus. Perferendis ipsam reprehenderit recusandae deleniti architecto. Molestias, dolores ipsam.</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio, nostrum?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, natus?</p>
         </div>
     );
 };
