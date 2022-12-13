87f5f7fb7a75f8d03db4ea521a81d8ed87eacc27
Midul-islam-Russell <midulraddy121@gmail.com>
2022-12-13 20:10:47 +0600

    add

--- src/component/Card/Card.js
+++ src/component/Card/Card.js
@@ -4,13 +4,7 @@ const Card = () => {
     return (
         <div>
             <h4>Hello Card</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, delectus?</p>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, quaerat?</p>\
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem totam praesentium, distinctio quibusdam expedita laudantium accusamus tempore laborum quaerat, non temporibus. Perferendis ipsam reprehenderit recusandae deleniti architecto. Molestias, dolores ipsam.</p>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio, nostrum?</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, natus?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, accusamus?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum libero asperiores </p>
+      
         </div>
     );
 };
